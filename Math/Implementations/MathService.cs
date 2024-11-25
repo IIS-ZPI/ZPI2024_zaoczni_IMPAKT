51c480114c86b379b1c44a91ede55c2ca360212b
Hubert Cywka <hcywka@gmail.com>
2024-11-25 22:09:40 +0100

    Implement addition

---
+++ Math/Implementations/MathService.cs
@@ -0,0 +1,11 @@
+﻿using IMPAKT.Math.Interfaces;
+
+namespace IMPAKT.Math.Implementations;
+
+class MathService : IMathService
+{
+    public double Addition(double a, double b)
+    {
+        return a + b;
+    }
+}
