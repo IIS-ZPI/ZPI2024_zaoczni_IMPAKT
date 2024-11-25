e4a55918fdf4cb078a82ae62b8c960fbf5537b79
lukasz-kkk <lukaluzny17@gmail.com>
2024-11-25 13:48:52 +0100

    Add information about myself (lukasz-kkk)

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Nazwa grupy: Impakt. Scrum master: tirey93.");
 Console.WriteLine("DevOps Engineer: hubert-cywka");
 Console.WriteLine("Dev1: tirey93");
+Console.WriteLine("Dev2: lukasz-kkk");
