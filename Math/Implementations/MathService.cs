4ef894834ac8905e5f56f9118ae9bddfc5f95120
Hubert Cywka <hubert.cywka@appfire.com>
2024-12-05 22:31:57 +0100

    Merge develop with arithmetic operations to main (#18)
    Co-authored-by: Damian Opolski - 202280 <202280@edu.p.lodz.pl>
    Co-authored-by: lukasz-kkk <lukaluzny17@gmail.com>
    Co-authored-by: Łukasz Kałużny <84326531+lukasz-kkk@users.noreply.github.com>
    Co-authored-by: Racid <48492931+BeastRacid@users.noreply.github.com>

---
+++ Math/Implementations/MathService.cs
@@ -0,0 +1,26 @@
+﻿using IMPAKT.Math.Interfaces;
+
+namespace IMPAKT.Math.Implementations;
+
+class MathService : IMathService
+{
+    public double Addition(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Difference(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplication(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Division(double a, double b)
+    {
+        return a / b;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Nazwa grupy: Impakt. Scrum master: tirey93 (developer).");
+using IMPAKT.Labs;
+using IMPAKT.Math.Implementations;
+using IMPAKT.Math.Interfaces;
+using IMPAKT.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+var serviceProvider = new ServiceCollection()
+    .Configure<MainSettings>(configuration.GetSection(nameof(MainSettings)))
+    .AddTransient<Lab1>()
+    .AddSingleton<IMathService, MathService>()
+    .BuildServiceProvider();
+
+var lab1 = serviceProvider.GetRequiredService<Lab1>();
+
+lab1.Execute();
