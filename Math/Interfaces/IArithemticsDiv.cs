bed67a2a0d38cb2c645944ee6fbe44ef16799525
Damian Opolski - 202280 <202280@edu.p.lodz.pl>
2024-12-13 12:42:11 +0100

    clean

--- Math/Interfaces/IArithemticsDiv.cs
+++
@@ -1,7 +0,0 @@
-namespace IMPAKT.Math.Interfaces;
-
-public interface IArithmeticsDiv
-{
-    // Below is the implementation of the division. Division divides 'a' by 'b'.
-    double Division(double a, double b);
-}
